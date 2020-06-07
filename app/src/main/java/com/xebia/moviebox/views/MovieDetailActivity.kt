3cfc071c7f4ae37cd12fbddc22ac1a44d9ee2d1b
DESKTOP-R5SQ9ER\Parag <parag.bhosale@zenwise.net>
2020-06-08 00:17:22 +0530

    Main Screen, Movie Detail UI Created

---
+++ app/src/main/java/com/xebia/moviebox/views/MovieDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.xebia.moviebox.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xebia.moviebox.R
+
+class MovieDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
